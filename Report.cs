2ab9972860f9992be36509ffb90366ee08e1bf3d
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-23 06:34:31 +0530

    Push report file

---
+++ Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class Report
+    {
+        /// <summary>
+        /// The Number of time dice was played to win position .
+        /// </summary>
+        public static void DiceThrowCount(int Position, int diceValue, int countDicePlay)
+        {
+            Console.WriteLine(" Number of Times Dice was played is : {0}", + countDicePlay);
+            Console.WriteLine("Position after {0} Die play is {1}",+countDicePlay, Position);
+        }
+    }
+}
