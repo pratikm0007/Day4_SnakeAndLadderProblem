6d7556cc37d6ce7b73e24581d10bc7850f0dbfe4
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-23 06:32:29 +0530

    Push ExactWin file

---
+++ ExactWin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class ExactWin
+    {
+        /// <summary>
+        /// This Ensure that player get exact position 100.
+        /// </summary>
+        public static void match(int Position, int diceValue, int countDicePlay)
+        {
+            Console.WriteLine("Check Exact win position 100 = True, otherwise False");
+            Console.WriteLine(Position.Equals(100));
+            Report.DiceThrowCount(Position, diceValue, countDicePlay);                        
+        }
+    }
+}
