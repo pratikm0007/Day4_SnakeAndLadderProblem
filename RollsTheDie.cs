d85a5ff155d18d82b7bf16288f8c23dac886de7b
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-23 06:23:41 +0530

    Push RollsTheDie file

---
+++ RollsTheDie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class RollsTheDie
+    {
+        public static int countDicePlay = 0;
+        /// <summary>
+        /// It gives dice value.
+        /// </summary>
+        public static void RollsDice(int Position)
+        {                
+            Random throwDice = new Random();
+            int diceValue = throwDice.Next(1, 7);
+            Console.WriteLine("Dice value is {0}", + diceValue);
+            countDicePlay++;
+            CheckForOption.SelectOption(Position, diceValue, countDicePlay);
+        }
+
+
+    }
+}
