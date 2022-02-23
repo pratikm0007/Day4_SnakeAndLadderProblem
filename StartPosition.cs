d143482513eeab1b5d5d83d145fa879d52e403a4
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-23 06:20:44 +0530

    Push start position file

---
+++ StartPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SnakeAndLadderProblem
+{
+    public class StartPosition
+    {
+        /// <summary>
+        /// Initialize position zero.
+        /// </summary>
+        public static void TakeStartPosition()
+        {
+            Console.WriteLine("Snake And Ladder Simulator");
+            int Position = 0;           
+            RollsTheDie.RollsDice(Position);
+        }
+    }
+}
